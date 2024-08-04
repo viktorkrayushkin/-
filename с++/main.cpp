17b7b72b60b2ce0f620011dc6fab8de796e2f60d
Viktor Krayushkin <gokertymgre68@gmail.com>
2024-08-04 18:50:11 +0300

    Initial Commit

---
+++ с++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  с++
+//
+//  Created by Виктор Краюшкин on 04.08.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
